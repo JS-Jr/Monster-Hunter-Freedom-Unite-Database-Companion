63b6f6258c37dd83c16bb33dcaefd89173a7755a
JS-Jr <130831855+JS-Jr@users.noreply.github.com>
2025-12-20 15:26:26 +0800

    add global marker icon

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+L.Marker.prototype.options.icon = L.icon({
+  iconUrl: markerIcon,
+  iconRetinaUrl: markerIcon2x,
+  shadowUrl: markerShadow,
+});
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />

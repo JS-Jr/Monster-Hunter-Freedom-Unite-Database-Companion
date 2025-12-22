ce0a95eefd70106e32dc62c19fd1d71775857783
JS-Jr <130831855+JS-Jr@users.noreply.github.com>
2025-12-22 17:41:05 +0800

    Refactor armor detail page

--- src/hooks/useDataFetch.ts
+++ src/hooks/useDataFetch.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 interface UseDataFetchOptions<T> {
   /**
@@ -75,7 +75,7 @@ export function useDataFetchArray<T>(
 
       try {
         // Test loading spiels
-        // await sleep(5000);
+        // await sleep(2000);
 
         // cache
         if (cache) {
@@ -147,15 +147,18 @@ export function useSingleDataFetch<T extends { name?: string; id?: string }>(
   url: string,
   identifier?: string,
   options: Omit<UseDataFetchOptions<T>, "filter"> = {}
-): UseDataFetchSingleResult<T> {
+) {
   const { data, loading, error } = useDataFetchArray<T>(url, options);
 
-  const singleItem =
-    data?.find(
-      (item) =>
-        item.name?.toLowerCase() === identifier?.toLowerCase() ||
-        item.id?.toLowerCase() === identifier?.toLowerCase()
-    ) || null;
+  const singleItem = useMemo(
+    () =>
+      data?.find(
+        (item) =>
+          item.name?.toLowerCase() === identifier?.toLowerCase() ||
+          item.id?.toLowerCase() === identifier?.toLowerCase()
+      ) || null,
+    [data, identifier]
+  );
 
   return { data: singleItem, loading, error };
 }

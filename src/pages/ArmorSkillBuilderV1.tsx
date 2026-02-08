018fc6b3f07f3b1cd3ad657cdf4f846890a6ca33
JS-Jr <130831855+JS-Jr@users.noreply.github.com>
2026-02-08 15:56:31 +0800

    Changed to raw html for proper redesigning

--- src/pages/ArmorSkillBuilderV1.tsx
+++ src/pages/ArmorSkillBuilderV1.tsx
@@ -22,75 +22,6 @@ type TableProps = {
 };
 
 function Table({ armorSkillBuilderDataProps }: TableProps) {
-  const SLOTS = [
-    // { type: "Helmet", label: "Helmet", selectPath: "/select/helmet" },
-    { type: "Helmet", label: "Helmet", selectPath: "/armor?type=helmet" },
-    {
-      type: "Helmet Decoration",
-      label: "Deocration",
-      selectPath: "/select/helmet/decoration",
-    },
-
-    { type: "Plate", label: "Plate", selectPath: "/armor?type=plate" },
-    {
-      type: "Plate Decoration",
-      label: "Deocration",
-      selectPath: "/select/plate/decoration",
-    },
-
-    {
-      type: "Gauntlets",
-      label: "Gauntlets",
-      selectPath: "/armor?type=gauntlets",
-    },
-    {
-      type: "Gauntlets Decoration",
-      label: "Deocration",
-      selectPath: "/select/gauntlets/decoration",
-    },
-
-    { type: "Waist", label: "Waist", selectPath: "/armor?type=waist" },
-    {
-      type: "Waist Decoration",
-      label: "Deocration",
-      selectPath: "/select/waist/decoration",
-    },
-
-    { type: "Leggings", label: "Leggings", selectPath: "/armor?type=leggings" },
-    {
-      type: "Leggings Decoration",
-      label: "Deocration",
-      selectPath: "/select/leggings/decoration",
-    },
-  ];
-
-  console.log("armorSkillBuilderData", armorSkillBuilderDataProps);
-
-  function getArmorSkillBuilderSingleItem(
-    armorType: string,
-    armorSkillBuilderData: ArmorSkillBuilder[],
-  ): ArmorSkillBuilder | undefined {
-    for (const amorSkillBuilderItem of armorSkillBuilderData) {
-      console.log(
-        "amorSkillBuilderItem.armor.type",
-        amorSkillBuilderItem.armor.type,
-      );
-      console.log("armorType", armorType);
-
-      if (amorSkillBuilderItem.armor.type.toLocaleLowerCase() == armorType) {
-        return amorSkillBuilderItem;
-      }
-
-      if (
-        amorSkillBuilderItem.armor.type.toLocaleLowerCase().includes(armorType)
-      ) {
-        return amorSkillBuilderItem;
-      }
-
-      return;
-    }
-  }
-
   return (
     <div className="overflow-x-auto">
       <table className="w-full bg-[#F7E7D0] rounded-lg shadow border-collapse">
@@ -103,220 +34,193 @@ function Table({ armorSkillBuilderDataProps }: TableProps) {
             <th className="px-4 py-3">Slots</th>
           </tr>
         </thead>
-
         <tbody>
-          {SLOTS.map((slotElement, index) => {
-            let slotType = slotElement.type.toLocaleLowerCase();
-            console.log(slotType);
-            let armor = getArmorSkillBuilderSingleItem(
-              slotType,
-              armorSkillBuilderDataProps,
-            );
-
-            let selectionActionRow: JSX.Element = (
-              <Link to={slotElement.selectPath}>
+          <tr className="border-t border-[#CBA986] hover:bg-[#F3DFC4]">
+            <td className="px-4 py-4 font-semibold">
+              <section>
+                <p>Helmet</p>
+              </section>
+            </td>
+            <td className="px-4 py-4 align-middle">
+              <a href="/armor?type=helmet" data-discover="true">
                 <button
                   className="px-4 py-2 rounded-md text-sm font-semibold
                    bg-[#6B3E1B] text-[#F7E7D0]
                    hover:bg-[#5A3215]
                    active:scale-95 transition-all"
                 >
-                  + Choose {slotElement.label}
+                  + Choose Helmet
                 </button>
-              </Link>
-            );
-
-            let skillRow: JSX.Element = (
+              </a>
+            </td>
+            <td className="px-4 py-4">-</td>
+            <td className="px-4 py-4 text-sm">
               <span className="italic text-[#8A6A4A]">—</span>
-            );
-
-            if (armor) {
-              selectionActionRow = (
-                <div className="flex items-center justify-between gap-2">
-                  <span className="font-medium">{armor.armor.name}</span>
-                  <div className="flex gap-2">
-                    <Link
-                      to={slotElement.selectPath}
-                      className="text-sm text-[#6B3E1B] hover:underline"
-                    >
-                      Change
-                    </Link>
-                    <button
-                      // onClick={() => removeSlot(slot.type)}
-                      className="text-sm text-red-600 hover:underline"
-                    >
-                      Remove
-                    </button>
-                  </div>
+            </td>
+            <td className="px-4 py-4">
+              <span className="italic text-[#8A6A4A]">—</span>
+            </td>
+          </tr>
+          Helmet Decoration
+          <tr className="border-t border-[#CBA986] hover:bg-[#F3DFC4]">
+            <td className="px-4 py-4 font-semibold">
+              <section>
+                <p>Plate</p>
+              </section>
+            </td>
+            <td className="px-4 py-4 align-middle">
+              <a href="/armor?type=plate" data-discover="true">
+                <button
+                  className="px-4 py-2 rounded-md text-sm font-semibold
+                   bg-[#6B3E1B] text-[#F7E7D0]
+                   hover:bg-[#5A3215]
+                   active:scale-95 transition-all"
+                >
+                  + Choose Plate
+                </button>
+              </a>
+            </td>
+            <td className="px-4 py-4">-</td>
+            <td className="px-4 py-4 text-sm">
+              <span className="italic text-[#8A6A4A]">—</span>
+            </td>
+            <td className="px-4 py-4">
+              <span className="italic text-[#8A6A4A]">—</span>
+            </td>
+          </tr>
+          Plate Decoration
+          <tr className="border-t border-[#CBA986] hover:bg-[#F3DFC4]">
+            <td className="px-4 py-4 font-semibold">
+              <section>
+                <p>Gauntlets</p>
+              </section>
+            </td>
+            <td className="px-4 py-4 align-middle">
+              <div className="flex items-center justify-between gap-2">
+                <span className="font-medium">Kut-Ku Vambraces X</span>
+                <div className="flex gap-2">
+                  <a
+                    className="text-sm text-[#6B3E1B] hover:underline"
+                    href="/armor?type=gauntlets"
+                    data-discover="true"
+                  >
+                    Change
+                  </a>
+                  <button className="text-sm text-red-600 hover:underline">
+                    Remove
+                  </button>
                 </div>
-              );
-
-              skillRow = (
-                <ul className="list-disc list-inside">
-                  {armor.armor.skills.map((skillItem, index) => (
-                    <li
-                      key={index}
-                      className={
-                        skillItem.positive ? "text-green-700" : "text-red-600"
-                      }
-                    >
-                      {skillItem.name} {skillItem.positive ? "+" : "-"}
-                      {skillItem.amount}
-                    </li>
-                  ))}
-                </ul>
-              );
-            }
-
-            if (!slotType.includes("decoration")) {
-              return (
-                <tr
-                  key={slotElement.type}
-                  className="border-t border-[#CBA986] hover:bg-[#F3DFC4]"
+              </div>
+            </td>
+            <td className="px-4 py-4">78</td>
+            <td className="px-4 py-4 text-sm">
+              <ul className="list-disc list-inside">
+                <li className="text-green-700">Rec Speed +1</li>
+                <li className="text-green-700">Attack +2</li>
+                <li className="text-red-600">Defence -2</li>
+                <li className="text-green-700">PsychicVis +4</li>
+              </ul>
+            </td>
+            <td className="px-4 py-4">2</td>
+          </tr>
+          <tr className="border-t border-[#CBA986] hover:bg-[#F3DFC4]">
+            <td className="px-4 py-4 font-semibold">
+              <section>
+                <p>Deocration</p>
+              </section>
+            </td>{" "}
+            <td className="px-4 py-4 align-middle">
+              <div>
+                <p className="font-medium">Attack Jewel</p>
+                <button className="text-sm text-[#6B3E1B] hover:underline">
+                  Change
+                </button>
+                <button className="text-red-600 text-sm hover:underline">
+                  Remove
+                </button>
+              </div>
+              <div>
+                <p className="font-medium">Map Jewel</p>
+                <button className="text-sm text-[#6B3E1B] hover:underline">
+                  Change
+                </button>
+                <button className="text-red-600 text-sm hover:underline">
+                  Remove
+                </button>
+              </div>
+            </td>
+            <td></td>
+            <td className="px-4 py-4 text-sm">
+              <ul className="list-disc list-inside">
+                <li className="text-green-700">Attack +1</li>
+              </ul>
+              <ul className="list-disc list-inside">
+                <li className="text-green-700">Map +2</li>
+              </ul>
+            </td>
+            <td className="px-4 py-4">
+              <span>
+                <p>1</p>
+              </span>
+              <span>
+                <p>1</p>
+              </span>
+            </td>
+          </tr>
+          <tr className="border-t border-[#CBA986] hover:bg-[#F3DFC4]">
+            <td className="px-4 py-4 font-semibold">
+              <section>
+                <p>Waist</p>
+              </section>
+            </td>
+            <td className="px-4 py-4 align-middle">
+              <a href="/armor?type=waist" data-discover="true">
+                <button
+                  className="px-4 py-2 rounded-md text-sm font-semibold
+                   bg-[#6B3E1B] text-[#F7E7D0]
+                   hover:bg-[#5A3215]
+                   active:scale-95 transition-all"
                 >
-                  <td className="px-4 py-4 font-semibold">
-                    <section>
-                      <p>{slotElement.label}</p>
-                    </section>
-                  </td>
-                  <td className="px-4 py-4 align-middle">
-                    {selectionActionRow}
-                  </td>
-                  <td className="px-4 py-4">
-                    {armor ? armor.armor.defense : "-"}
-                  </td>
-                  <td className="px-4 py-4 text-sm">{skillRow}</td>
-                  <td className="px-4 py-4">
-                    {armor ? (
-                      armor.armor.slots
-                    ) : (
-                      <span className="italic text-[#8A6A4A]">—</span>
-                    )}
-                  </td>
-                </tr>
-              );
-            }
-
-            slotType = SLOTS[index - 1].type.toLocaleLowerCase();
-            console.log(slotType);
-            armor = getArmorSkillBuilderSingleItem(
-              slotType,
-              armorSkillBuilderDataProps,
-            );
-
-            console.log("armor", armor);
-            if (armor?.attachedDecoration === undefined) {
-              console.log("attachedDecoration is undefined");
-              return <>{slotElement.type}</>;
-            }
-
-            const attachedDecoration: Decoration[] = armor.attachedDecoration;
-
-            console.log("attachedDecoration", attachedDecoration);
-
-            function getAttachedDecorationNames(
-              decorationData: Decoration[],
-            ): JSX.Element {
-              const content: JSX.Element[] = decorationData.map(
-                (decorationDataItem, index) => (
-                  <span key={index}>
-                    <p>{decorationDataItem.name}</p>
-                  </span>
-                ),
-              );
-
-              return <>{content}</>;
-            }
-
-            function getAttachedDecorationSkills(
-              decorationData: Decoration[],
-            ): JSX.Element {
-              const content: JSX.Element[] = decorationData.map(
-                (decorationDataItem, index) => (
-                  <ul className="list-disc list-inside" key={index}>
-                    {decorationDataItem.skills.map((skillItem, skillIndex) => (
-                      <li
-                        key={skillIndex} // Use skillIndex for unique keys within the list
-                        className={
-                          skillItem.positive ? "text-green-700" : "text-red-600"
-                        }
-                      >
-                        {skillItem.name} {skillItem.positive ? "+" : "-"}
-                        {skillItem.amount}
-                      </li>
-                    ))}
-                  </ul>
-                ),
-              );
-
-              return <>{content}</>;
-            }
-
-            {
-              armor.armor.skills.map((skillItem, index) => (
-                <li
-                  key={index}
-                  className={
-                    skillItem.positive ? "text-green-700" : "text-red-600"
-                  }
+                  + Choose Waist
+                </button>
+              </a>
+            </td>
+            <td className="px-4 py-4">-</td>
+            <td className="px-4 py-4 text-sm">
+              <span className="italic text-[#8A6A4A]">—</span>
+            </td>
+            <td className="px-4 py-4">
+              <span className="italic text-[#8A6A4A]">—</span>
+            </td>
+          </tr>
+          Waist Decoration
+          <tr className="border-t border-[#CBA986] hover:bg-[#F3DFC4]">
+            <td className="px-4 py-4 font-semibold">
+              <section>
+                <p>Leggings</p>
+              </section>
+            </td>
+            <td className="px-4 py-4 align-middle">
+              <a href="/armor?type=leggings" data-discover="true">
+                <button
+                  className="px-4 py-2 rounded-md text-sm font-semibold
+                   bg-[#6B3E1B] text-[#F7E7D0]
+                   hover:bg-[#5A3215]
+                   active:scale-95 transition-all"
                 >
-                  {skillItem.name} {skillItem.positive ? "+" : "-"}
-                  {skillItem.amount}
-                </li>
-              ));
-            }
-            function getAttachedDecorationSlotConsumption(
-              decorationData: Decoration[],
-            ): JSX.Element {
-              const content: JSX.Element[] = decorationData.map(
-                (decorationDataItem, index) => (
-                  <span key={index}>
-                    <p>{decorationDataItem.slots}</p>
-                  </span>
-                ),
-              );
-
-              return <>{content}</>;
-            }
-
-            let slotRow = (
-              <>
-                {armor?.attachedDecoration ? (
-                  armor.armor.slots
-                ) : (
-                  <span className="italic text-[#8A6A4A]">—</span>
-                )}
-              </>
-            );
-
-            selectionActionRow = getAttachedDecorationNames(attachedDecoration);
-            skillRow = getAttachedDecorationSkills(attachedDecoration);
-            slotRow = getAttachedDecorationSlotConsumption(attachedDecoration);
-
-            return (
-              <tr
-                key={slotElement.type}
-                className="border-t border-[#CBA986] hover:bg-[#F3DFC4]"
-              >
-                <td className="px-4 py-4 font-semibold">
-                  <section>
-                    <p>{slotElement.label}</p>
-                  </section>
-                </td>
-                <td className="px-4 py-4 align-middle">{selectionActionRow}</td>
-                <td></td>
-                <td className="px-4 py-4 text-sm">{skillRow}</td>
-                <td className="px-4 py-4">
-                  {/* {armor?.attachedDecoration ? (
-                    armor.armor.slots
-                  ) : (
-                    <span className="italic text-[#8A6A4A]">—</span>
-                  )} */}
-                  {slotRow}
-                </td>
-              </tr>
-            );
-          })}
+                  + Choose Leggings
+                </button>
+              </a>
+            </td>
+            <td className="px-4 py-4">-</td>
+            <td className="px-4 py-4 text-sm">
+              <span className="italic text-[#8A6A4A]">—</span>
+            </td>
+            <td className="px-4 py-4">
+              <span className="italic text-[#8A6A4A]">—</span>
+            </td>
+          </tr>
+          Leggings Decoration
         </tbody>
       </table>
     </div>
@@ -351,11 +255,6 @@ function SummaryTable() {
 }
 
 export default function ArmorSkillBuilderV1() {
-  // const data = getArmorSkillBuilderData();
-  // console.log("data", data);
-
-  // const [armorSkillBuilder, setArmorSkillBuilder] = useState();
-
   const armorMapper = useCallback(
     (rawData: ArmorJson[]) => rawData.map(mapRawArmorToArmor),
     [],

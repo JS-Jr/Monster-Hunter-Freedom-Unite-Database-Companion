67730533bd4dde12cc6cfebab0f0d3eed19f5ec7
JS-Jr <130831855+JS-Jr@users.noreply.github.com>
2026-02-18 18:29:40 +0800

    refactor links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,55 +4,35 @@ import { useState } from "react";
 export default function Header() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
+  const links = [
+    { path: "/", label: "Home" },
+    { path: "/monsters", label: "Monsters" },
+    { path: "/armor", label: "Armor" },
+    { path: "/weapons", label: "Weapons" },
+    { path: "/maps", label: "Maps" },
+    { path: "/weapons/tree", label: "Weapon Tree" },
+    { path: "/item", label: "Item" },
+    { path: "/decorations", label: "Decoration" },
+    { path: "/skills", label: "Skill" },
+    { path: "/skill-builder", label: "Armor Skill Builder" },
+    { path: "/status", label: "Status/State" },
+  ];
+
   return (
     <header className="h-16 w-full bg-[#3b2f24] text-[#f7e7d0] flex items-center justify-between px-6 shadow-md">
       {/* Logo / Title */}
       <h2 className="text-lg font-bold tracking-wide">Monster Hunter DB</h2>
 
-      {/* Nav links */}
       <nav className="flex gap-6 text-sm font-medium relative">
-        <Link className="hover:text-[#d0b38a] transition-colors" to="/">
-          Home
-        </Link>
-        <Link className="hover:text-[#d0b38a] transition-colors" to="/monsters">
-          Monsters
-        </Link>
-        <Link className="hover:text-[#d0b38a] transition-colors" to="/armor">
-          Armor
-        </Link>
-        <Link className="hover:text-[#d0b38a] transition-colors" to="/weapons">
-          Weapons
-        </Link>
-        <Link className="hover:text-[#d0b38a] transition-colors" to="/maps">
-          Maps
-        </Link>
-        <Link
-          className="hover:text-[#d0b38a] transition-colors"
-          to="/weapons/tree"
-        >
-          Weapon Tree
-        </Link>
-        <Link className="hover:text-[#d0b38a] transition-colors" to="/item">
-          Item
-        </Link>
-        <Link
-          className="hover:text-[#d0b38a] transition-colors"
-          to="/decorations"
-        >
-          Decoration
-        </Link>
-        <Link className="hover:text-[#d0b38a] transition-colors" to="/skills">
-          Skill
-        </Link>
-        <Link
-          className="hover:text-[#d0b38a] transition-colors"
-          to="/skill-builder"
-        >
-          Armor Skill Builder
-        </Link>
-        <Link className="hover:text-[#d0b38a] transition-colors" to="/status">
-          Status/State
-        </Link>
+        {links.map((link) => (
+          <Link
+            className="hover:text-[#d0b38a] transition-colors"
+            to={link.path}
+            key={link.label}
+          >
+            {link.label}
+          </Link>
+        ))}
         <div
           className="relative"
           onMouseEnter={() => setIsDropdownOpen(true)}

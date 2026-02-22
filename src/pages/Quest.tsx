969a4635af1f6bfaeb0b50427e49af377a21d4b7
JS-Jr <130831855+JS-Jr@users.noreply.github.com>
2026-02-22 17:29:30 +0800

    adjust table and used deep search

--- src/pages/Quest.tsx
+++ src/pages/Quest.tsx
@@ -1,12 +1,20 @@
 import type { Quest } from "../types/Quest";
-import { createColumnHelper } from "@tanstack/react-table";
+import { createColumnHelper, type FilterFn } from "@tanstack/react-table";
 import { Table } from "../components/Table";
 import { TableEmptyState } from "../components/TableEmptyState";
 import { TableSkeleton } from "../components/TableSkeletonProps";
 import { useDataFetchArray } from "../hooks/useDataFetch";
 import { useUrlFilters } from "../hooks/useUrlFilters";
+import deepSearch from "../function/deepSearch";
 
 export default function Quest() {
+  const deepQuestGlobalFilter: FilterFn<Quest> = (row, _, filterValue) => {
+    const search = String(filterValue).toLowerCase().trim();
+    if (!search) return true;
+
+    return deepSearch(row.original, search);
+  };
+
   const {
     columnFilters,
     globalFilter,
@@ -16,17 +24,12 @@ export default function Quest() {
     handleSortingChange,
   } = useUrlFilters();
 
-  const { data: quests, loading } = useDataFetchArray<Quest>("/data/quest.json");
+  const { data: quests, loading } =
+    useDataFetchArray<Quest>("/data/quest.json");
 
   const columnHelper = createColumnHelper<Quest>();
 
   const questColumns = [
-    columnHelper.accessor("name", {
-      header: "Quest Name",
-      cell: ({ getValue }) => (
-        <span className="font-semibold text-[#5A3F28]">{getValue()}</span>
-      ),
-    }),
     columnHelper.accessor("category", {
       header: "Category",
       filterFn: "equalsString",
@@ -35,12 +38,31 @@ export default function Quest() {
         options: Array.from(new Set(quests?.map((quest) => quest.category))),
       },
     }),
+    columnHelper.accessor("name", {
+      header: "Quest Name",
+      cell: ({ getValue }) => (
+        <span className="font-semibold text-[#5A3F28]">{getValue()}</span>
+      ),
+    }),
+
     columnHelper.accessor("quest-type", {
       header: "Type",
       filterFn: "equalsString",
       meta: {
         type: "select",
-        options: Array.from(new Set(quests?.map((quest) => quest["quest-type"]))),
+        options: Array.from(
+          new Set(quests?.map((quest) => quest["quest-type"])),
+        ),
+      },
+    }),
+    columnHelper.accessor("difficulty", {
+      header: "Difficulty",
+      filterFn: "equalsString",
+      meta: {
+        type: "select",
+        options: Array.from(
+          new Set(quests?.map((quest) => quest.difficulty)),
+        ).sort((a, b) => a - b),
       },
     }),
     columnHelper.accessor("reward", {
@@ -48,9 +70,6 @@ export default function Quest() {
     }),
     columnHelper.accessor("contract-fee", {
       header: "Contract Fee",
-    }),
-    columnHelper.accessor("time-limit", {
-      header: "Time Limit",
     }),
     columnHelper.accessor("location", {
       header: "Location",
@@ -66,17 +85,6 @@ export default function Quest() {
     }),
     columnHelper.accessor("goal-condition", {
       header: "Goal",
-    }),
-    columnHelper.accessor("client", {
-      header: "Client",
-    }),
-    columnHelper.accessor("difficulty", {
-      header: "Difficulty",
-      filterFn: "equalsString",
-      meta: {
-        type: "select",
-        options: Array.from(new Set(quests?.map((quest) => quest.difficulty))).sort((a, b) => a - b),
-      },
     }),
   ];
 
@@ -106,6 +114,7 @@ export default function Quest() {
         columns={questColumns}
         initialPageSize={10}
         globalFilterable
+        globalFilterFn={deepQuestGlobalFilter}
         initialColumnFilters={columnFilters}
         onFiltersChange={handleFiltersChange}
         initialGlobalFilter={globalFilter}

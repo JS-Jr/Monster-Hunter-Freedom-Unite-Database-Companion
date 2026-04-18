547d29351c24213e0eb90fb4e860676d538b43b4
JS-Jr <130831855+JS-Jr@users.noreply.github.com>
2026-04-18 16:54:01 +0800

    fix armor path reference

--- src/hooks/importJsonFile.ts
+++ src/hooks/importJsonFile.ts
@@ -1,15 +1,8 @@
 import { type Armor } from "../types/Armor";
 import mapArmor from "../utils/mapArmor";
-import armorData from "../../public/data/armors.json";
+import armorData from "../data/armors.json";
 
 export function ArmorLoader(): Armor[] {
   const raw = armorData as any[];
-
-  // function sleep(ms: number) {
-  //   return new Promise((resolve) => setTimeout(resolve, ms));
-  // }
-  // console.log("Loading for 3 seconds to show skeleton");
-  // await sleep(3000);
-
   return raw.map(mapArmor) as Armor[];
 }
